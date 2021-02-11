742e7a9b38b5679c0674abde981fdfb61e12f7ae
Alex Sousa <alxroots@gmail.com>
2021-02-11 11:57:10 -0300

    few updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
-import ToastContainer from './components/ToastContainer';
 import AppProvider from './hooks';
 import Routes from './routes';
 
